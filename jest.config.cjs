97ad96dd10d9ca14257828ed35eab1132d112815
Chris Ackerman <chris@topher.land>
2022-09-10 11:57:51 -0700

    Updated dependencies

--- jest.config.cjs
+++ jest.config.cjs
@@ -16,14 +16,11 @@ module.exports = {
   coverageReporters: ['text-summary', 'html-spa', 'lcov'],
   coverageThreshold: { global: { branches: 80, functions: 80, lines: 80, statements: 80 } },
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  globals: {
-    'ts-jest': { useESM: true },
-  },
   moduleNameMapper: {
     // Remove the .js extension (required for ES Module support) from TS file imports.
     '^(\\.{1,2}/.*)\\.jsx?$': '$1',
   },
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   restoreMocks: true,
   roots: ['<rootDir>'],
   setupFilesAfterEnv: [],
